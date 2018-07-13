400c596649af9fc2cc555b1e92816a2214f0514f
Erwan Moreau <moreaue@scss.tcd.ie>
2018-07-13 17:08:11 +0100

    stuff

--- setup-path.sh
+++ setup-path.sh
@@ -4,5 +4,6 @@ rootVMWE=$(pwd)
 
 cd code; make; cd ..
 
-export PATH=$PATH:$rootVMWE/code:$rootVMWE/scripts:$rootVMWE/seq/scripts:$rootVMWE/dep-tree/scripts/
+export PATH=$PATH:$rootVMWE/code:$rootVMWE/scripts:$rootVMWE/seq/scripts:$rootVMWE/dep-tree/scripts/:$rootVMWE/shared-task-scripts/
+
 
